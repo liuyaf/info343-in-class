17321988da2068ecb812c88289380943a0381a40
Liuyang Fu <zzl19970122@gmail.com>
2017-10-13 11:43:57 -0700

    string practices

--- labs/javascript/lab.js
+++ labs/javascript/lab.js
@@ -1,2 +1,12 @@
 // Put your JavaScript lab code here!
 
+let string = "I like JavaScript more than I like to party";
+console.log(string.slice(-15));
+
+let wsu = "I love Wasthington State University";
+let husky = wsu.replace("State", "");
+console.log(husky);
+
+let combined = string.concat(husky);
+console.log(combined);
+
